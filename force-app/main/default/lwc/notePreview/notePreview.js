f3aa3294f2fca285e92390b48d42435204125371
Achal Urankar <achalurankar@gmail.com>
2025-05-26 00:28:43 +0530

    retrieved package

--- force-app/main/default/lwc/notePreview/notePreview.js
+++ force-app/main/default/lwc/notePreview/notePreview.js
@@ -1,6 +1,7 @@
 import { LightningElement, api, wire } from 'lwc';
 import { publish, MessageContext } from 'lightning/messageService';
 import noteSelectedChannel from '@salesforce/messageChannel/noteSelectedChannel__c';
+import noteDeletedChannel from '@salesforce/messageChannel/noteDeletedChannel__c';
 
 export default class NotePreview extends LightningElement {
 
@@ -11,5 +12,9 @@ export default class NotePreview extends LightningElement {
 
     selectNote(event) {
         publish(this.messageContext, noteSelectedChannel, this.note);
+    }
+
+    deleteNote() {
+        publish(this.messageContext, noteDeletedChannel, this.note);
     }
 }
